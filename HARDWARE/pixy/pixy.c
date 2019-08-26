caf0a26b40a904bbaf34d0859f35bbdcd66e31a2
Administrator <1375285254@qq.com>
2019-08-26 12:53:34 +0800

    first commit

---
+++ HARDWARE/pixy/pixy.c
@@ -0,0 +1,61 @@
+#include "pixy.h"
+#include "timer.h"
+#include "led.h"
+
+//配套使用CMUCAM5(Pixy)解码
+
+
+Pixy_Color Pixy_Color_Inf;//结构体例化
+u8 Raw_Data[40];//原始数据大包，为解串准备
+u8 counter;//计数用
+
+
+
+/**********************STM32F4XX UART1初始化程序********************************/
+//PA9，PA10复用为UART1
+//数据位8，停止位1，无其它位,波特率19200
+//使用串口中断方式,不使用可以注释掉后6行代码
+/*******************************END*********************************************/
+void Pixy_Uart_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	
+	RCC_AHBPeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE); //使能GPIOA时钟
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);//使能USART1时钟
+ 
+	//串口1对应引脚复用映射
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource9,GPIO_AF_USART1); //GPIOA9复用为USART1
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource10,GPIO_AF_USART1); //GPIOA10复用为USART1
+	
+	//USART1端口配置
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10; //GPIOA9与GPIOA10
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;//复用功能
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	//速度50MHz
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP; //推挽复用输出
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP; //上拉
+	GPIO_Init(GPIOA,&GPIO_InitStructure); //初始化PA9，PA10
+
+   //USART1 初始化设置
+	USART_InitStructure.USART_BaudRate = 500000;//波特率设置
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;//字长为8位数据格式
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;//一个停止位
+	USART_InitStructure.USART_Parity = USART_Parity_No;//无奇偶校验位
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无硬件数据流控制
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//收发模式
+    USART_Init(USART1, &USART_InitStructure); //初始化串口1
+	
+    USART_Cmd(USART1, ENABLE);  //使能串口1 
+	
+	USART_ClearFlag(USART1, USART_FLAG_TC);
+	
+	//使用中断方式
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//开启相关中断
+	//Usart1 NVIC 配置
+  NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;//串口1中断通道
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=3;//抢占优先级3
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority =3;		//子优先级3
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
+	NVIC_Init(&NVIC_InitStructure);	//根据指定的参数初始化VIC寄存器、
+}
